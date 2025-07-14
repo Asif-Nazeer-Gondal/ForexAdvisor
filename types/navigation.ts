4c72d42170a42d8e5b6aa970af2e1f4db3d19399
Asif-Nazeer-Gondal <gondalbadshah786786@gmail.com>
2025-07-14 19:35:07 +0500

    suggested files added

--- types/navigation.ts
+++ types/navigation.ts
@@ -1,7 +1,8 @@
 // types/navigation.ts
+
 export type RootStackParamList = {
   Home: undefined;
   Budget: undefined;
-  Predictor: undefined;
   Settings: undefined;
+  BudgetCalculator: undefined;
 };
