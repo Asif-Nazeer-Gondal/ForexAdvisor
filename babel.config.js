1d4121e2b4c2ef81482428a2137e282d2affa752
Asif-Nazeer-Gondal <gondalbadshah786786@gmail.com>
2025-07-18 00:37:33 +0500

    files are modified

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
-﻿module.exports = function (api) {
+﻿import Constants from 'expo-constants';
+import { OPENAI_API_KEY } from '@env';
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       "nativewind/babel",
-      ["babel-plugin-styled-components", { "ssr": false }]
+      ["babel-plugin-styled-components", { "ssr": false }],
+      ["module:react-native-dotenv"]
     ]
   };
 };
